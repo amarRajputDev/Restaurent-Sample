3c2ccf3677a9af845be8fdba3a4ba7f34ebca743
amarRajputDev <amarrajputvines@gmail.com>
2024-09-16 11:33:55 +0530

    ani

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,29 @@
 "use client"
 
 import dynamic from 'next/dynamic'
+import Lenis from 'lenis';
+import { useEffect } from 'react';
+
 
 const FoodieLandingPage = dynamic(() => import('../components/ui/Rep'), { ssr: false })
 
+
+
 export default function Home() {
+  const lenis = new Lenis()
+useEffect(() => {
+
+lenis.on('scroll', (e) => {
+  console.log(e)
+})
+
+function raf(time: any) {
+  lenis.raf(time)
+  requestAnimationFrame(raf)
+}
+
+requestAnimationFrame(raf)
+}, []);
   return (
     <div>
       <FoodieLandingPage />
