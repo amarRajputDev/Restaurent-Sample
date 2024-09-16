1dd7f38ddfe96be170404d06cea9e9728165d051
amarRajputDev <amarrajputvines@gmail.com>
2024-09-16 13:22:45 +0530

    deploy error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,29 @@
 "use client"
 
 import dynamic from 'next/dynamic'
-import Lenis from 'lenis';
 import { useEffect } from 'react';
-
 
 const FoodieLandingPage = dynamic(() => import('../components/ui/Rep'), { ssr: false })
 
-
-
 export default function Home() {
-  const lenis = new Lenis()
-useEffect(() => {
+  useEffect(() => {
+    // Move Lenis initialization inside useEffect
+    import('lenis').then(({ default: Lenis }) => {
+      const lenis = new Lenis()
 
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
+      lenis.on('scroll', (e: any) => {
+        console.log(e)
+      })
 
-function raf(time: any) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
-}
+      function raf(time: any) {
+        lenis.raf(time)
+        requestAnimationFrame(raf)
+      }
 
-requestAnimationFrame(raf)
-}, []);
+      requestAnimationFrame(raf)
+    })
+  }, []);
+
   return (
     <div>
       <FoodieLandingPage />
